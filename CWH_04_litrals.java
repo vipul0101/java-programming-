8b13b1389d82b171e7e8effbd79341bc09379f83
vipul <vipulgupta8383015057@gmail.com>
2023-04-07 18:14:52 +0530

    i have add chapter 4 of codewith harry java tutorials

---
+++ CWH_04_litrals.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CWH_04_litrals {
+    public static void main(String[] args) {
+        byte age = 36;
+        int num = 55;
+        short a = 78;
+        long b = 4555555555578L;
+        char f = 'A';
+        float d = 0.6f;
+        double d1 = 5.66d;
+        boolean b1 = true;
+        String str = "harry";
+        System.out.println(str);
+
+    }
+}
