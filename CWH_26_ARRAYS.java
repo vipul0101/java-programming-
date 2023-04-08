c6323becf9fd34f4a4ade889c79fc1b92354367f
vipul <vipulgupta8383015057@gmail.com>
2023-04-08 17:20:59 +0530

    day 26

---
+++ CWH_26_ARRAYS.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class CWH_26_ARRAYS {
+    public static void main(String[] args) {
+        /* classroom of 500 students - you have to show the marks of these students
+        two methods are there
+        1- use 500 variables
+        2- use the method of arrays (recommended)
+         */
+        // how to code arrays
+        int [] marks = new int [5];
+        marks[1] = 50;
+        marks[2] = 40;
+        marks[3] = 89;
+        marks[4] = 55;
+        marks[0] = 99;
+        System.out.println(marks[4]);
+
+    }
+}
---
+++ CWH_27_FOREACHLOOP.java
@@ -0,0 +1,2 @@
+package com.company;public class CWH_27_FOREACHLOOP {
+}
